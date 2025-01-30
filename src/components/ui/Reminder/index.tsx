eb332b50cf5c9cb2286309b89dc1866647374eb3
nikita-osipov59 <67689788+nikita-osipov59@users.noreply.github.com>
2025-01-30 17:51:07 +0300

    [u] Reminder

--- src/components/ui/Reminder/index.tsx
+++ src/components/ui/Reminder/index.tsx
@@ -1,4 +1,4 @@
-import { Button, LinkBtn } from "@/components/ui";
+import { LinkBtn } from "@/components/ui";
 
 import { ReminderProps } from "./ReminderProps.props";
 
@@ -6,34 +6,24 @@ import { ROUTER_PATH } from "@/router/PATH";
 
 import style from "./style.module.scss";
 
-export const Reminder: React.FC<ReminderProps> = ({ onClick, view }) => {
+export const Reminder: React.FC<ReminderProps> = ({ view }) => {
   return (
     <>
       {view === "Registration" && (
-        <>
-          <Button onClick={onClick} width="100%" variant="primary">
-            Registration
-          </Button>
-          <p className={style.reminder}>
-            Have an account?
-            <LinkBtn to={ROUTER_PATH.AUTH} variant="default">
-              Login
-            </LinkBtn>
-          </p>
-        </>
+        <p className={style.reminder}>
+          Have an account?
+          <LinkBtn to={ROUTER_PATH.AUTH} variant="default">
+            Login
+          </LinkBtn>
+        </p>
       )}
       {view === "Login" && (
-        <>
-          <Button onClick={onClick} width="100%" variant="primary">
-            Login
-          </Button>
-          <p className={style.reminder}>
-            Dont have an account?
-            <LinkBtn to={ROUTER_PATH.REGISTRATION} variant="default">
-              Registration
-            </LinkBtn>
-          </p>
-        </>
+        <p className={style.reminder}>
+          Dont have an account?
+          <LinkBtn to={ROUTER_PATH.REGISTRATION} variant="default">
+            Registration
+          </LinkBtn>
+        </p>
       )}
     </>
   );
