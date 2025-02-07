9e5a49034450fc2e1c6570768feea4a94ba58304
nikita-osipov59 <67689788+nikita-osipov59@users.noreply.github.com>
2025-02-07 18:21:52 +0300

    [+] TanStack Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { queryClient } from "./api/query-client.ts";
 
 import App from "./App.tsx";
 
@@ -7,6 +11,9 @@ import "@/styles/index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </BrowserRouter>
 );
